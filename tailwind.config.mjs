92db9435a337625212e74fbd44d8d9d22211a389
Lukas Diessner <lukas.diessner123@gmail.com>
2024-10-23 23:31:53 +0200

    Start of development

---
+++ tailwind.config.mjs
@@ -0,0 +1,39 @@
+import { addDynamicIconSelectors } from "@iconify/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+   theme: {
+      extend: {},
+      colors: {
+         light: "#e7eef9",
+         dark: "#090909",
+         primary: "#3473b7",
+         secondary: "#321d67",
+         accent: "#5e278b",
+      },
+   },
+   plugins: [addDynamicIconSelectors()],
+};
+
+/*
+
+--Dark
+colors: {
+         text: "#e7eef9",
+         background: "#010304",
+         primary: "#3473b7",
+         secondary: "#321d67",
+         accent: "#5e278b",
+      },
+
+--Light
+colors: {
+   'text': '#060d18',
+   'background': '#fbfdfe',
+   'primary': '#4888cb',
+   'secondary': '#ad98e2',
+   'accent': '#ab74d8',
+},
+
+*/
